d537e808492952f8317fe1653db08a3dd5dcf337
zifkage <zifkage@gmail.com>
2017-11-12 16:37:34 +0100

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    username: 'Zifkage',
+    text: 'Salut humain'
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
